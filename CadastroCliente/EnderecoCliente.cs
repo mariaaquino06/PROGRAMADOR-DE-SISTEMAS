a8ee34af664d9f053e5d40ecf8f784c8270d6476
MARIA EDUARDA OLIVEIRA AQUINO <maria.eoaquino@senacsp.edu.br>
2025-03-17 22:16:33 -0300

    Cadastro Cliente

---
+++ CadastroCliente/EnderecoCliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroCliente
+{
+    internal class EnderecoCliente
+    {
+        public string logradouro;
+        public string numero;
+        public string complemento;
+        public string bairro;
+        public string cidade;
+        public string estado;
+        public string cep; 
+
+    }
+}
